c695fd45e067bd9dd2a372e75cf544284150bb61
Gilles Cédric <cedric.nguefack@tapmeppe.com>
2020-08-01 01:49:58 +0100

    restructuration

--- src/layouts/Admin/Admin.js
+++ src/layouts/Admin/Admin.js
@@ -116,7 +116,7 @@ class Admin extends React.Component {
             routes={routes}
             bgColor={this.state.backgroundColor}
             logo={{
-              outterLink: "https://www.smart-trash.neway-agency.com/",
+              outterLink: "https://smart-trash.neway-agency.com/",
               text: "Smart-Trash",
               imgSrc: logo
             }}
